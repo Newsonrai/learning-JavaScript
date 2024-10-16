2d6aea6290285ebef54d20b94b2c94bdfdf156de
Newson Rai <firstnepseking@gmail.com>
2024-10-16 17:18:10 +0545

    first commit

---
+++ varaibles.js
@@ -0,0 +1,34 @@
+//varaibles in JavaScript are:-
+// var cup="Tea";
+// let cup="Tea";
+// const cup="Tea";
+// let cup="Tea";
+// cup="milk";
+// console.log(cup);
+
+// if (true) {
+//     var name="John Doe";
+//     console.log(name);
+// }
+// console.log(name);
+
+// const age=24;
+// const isMarried=false;
+// console.log(age,isMarried);
+
+// let name="Newson"
+// let age=24
+// console.log(name + " Rai" + " " +age)
+
+let friends=["John","Jane","Jill"];
+console.log(friends);
+console.log(friends[0]);
+
+let package={
+    enrollmentFee:1000,
+    name:"Newson",
+    city:"Kathmandu",
+    enrolledCourse:"nodeJS"
+}
+console.log(package.enrollmentFee,package.name,package.city,package.enrolledCourse);
+
