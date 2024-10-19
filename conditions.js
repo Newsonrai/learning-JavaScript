33f20271f2b471a2b55e908bdad425304584b2bd
Newson Rai <firstnepseking@gmail.com>
2024-10-19 23:54:21 +0545

    Conditional statemnet in JavaScript

---
+++ conditions.js
@@ -0,0 +1,48 @@
+// Conditional statement  in JavaScript.
+
+// 1.if statement
+// 2.if-else statement
+
+// const isRaining=true;
+
+// if(isRaining){
+//     console.log("Take an umbrella");
+// }else{
+//     console.log("No need to take an umbrella");
+// }
+
+// const temperature=31;
+
+// if(temperature>30){
+//     console.log("It's a hot day");
+// }else if(temperature<20){
+//     console.log("It's a cold day");
+// }else{
+//     console.log("It's neither hot nor cold");
+// }
+
+// const age=15
+// if (age==18){
+//     console.log("You are elligible to vote");
+// }else{
+//     console.log("You are not elligible to vote");
+// }
+
+// const isRaining=false;
+
+// ternary operator in JavaScript
+
+// const result=isRaining?"Take an umbrella":"No need to take an umbrella";
+// console.log(result);
+
+// short circuit operator in JavaScript
+
+// const result=isRaining && "Take an umbrella" || "No need to take an umbrella";
+// console.log(result)
+
+// Nullish coeliscing operator in JavaScript
+
+let age=null;
+
+const result=age ?? "No age given"
+console.log(result)
